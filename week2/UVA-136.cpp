d742d2af2a396eb5b4ececaea4bfeb08efbb7819
uno <st9007a@gmail.com>
2018-03-15 21:17:05 +0800

    Add uva 136 and fix compiler options

--- week2/UVA-136.cpp
+++ week2/UVA-136.cpp
@@ -1,6 +1,28 @@
+#include <algorithm>
 #include <cstdio>
+
+using namespace std;
 
 int main()
 {
+    long long lists[1500];
+    int n2 = 0, n3 = 0, n5 = 0;
 
+    lists[0] = 1;
+
+    for (int i = 1; i < 1500; i++) {
+        while (lists[n2] * 2 <= lists[i - 1]) {
+            n2++;
+        }
+        while (lists[n3] * 3 <= lists[i - 1]) {
+            n3++;
+        }
+        while (lists[n5] * 5 <= lists[i - 1]) {
+            n5++;
+        }
+
+        lists[i] = min(lists[n2] * 2, min(lists[n3] * 3, lists[n5] * 5));
+    }
+
+    printf("The 1500'th ugly number is %lld.\n", lists[1499]);
 }
